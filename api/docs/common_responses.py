ad9ea25b1421947e14e53dd7465e77c5128efd88
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2025-11-11 07:52:32 +0100

    Remove 500 error from common response definitions

--- api/docs/common_responses.py
+++ api/docs/common_responses.py
@@ -1,18 +1,15 @@
 RESOURCE_ERRORS = [
     (401, "Unauthorized access."),
     (404, "Resource not found."),
-    (500, "Internal server error."),
 ]
 
 CREATION_ERRORS = [
     (401, "Unauthorized access."),
     (409, "Resource already exists."),
-    (500, "Internal server error."),
 ]
 
 LISTING_ERRORS = [
     (401, "Unauthorized access."),
-    (500, "Internal server error."),
 ]
 
 def apply_common_responses(blp, responses):
