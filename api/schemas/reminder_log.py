f9931d320d3ad603be572e56ede53443483eb765
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2025-11-13 17:26:50 +0100

    Remove required=True from subscription_id in ReminderLogSchema

--- api/schemas/reminder_log.py
+++ api/schemas/reminder_log.py
@@ -2,7 +2,7 @@ from marshmallow import Schema, fields
 
 class ReminderLogSchema(Schema):
     id = fields.Int(dump_only=True)
-    subscription_id = fields.Int(required=True, dump_only=True)
+    subscription_id = fields.Int(dump_only=True)
     message = fields.Str()
     success = fields.Bool(required=True)
     sent_at = fields.DateTime(format="%Y-%m-%d %H:%M:%S", dump_only=True)
