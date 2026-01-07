7ae3566d18607d76e59a23359765260edc31ded8
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2026-01-07 20:04:33 +0100

    Optimize Redis client usage by introducing per-process singleton

--- api/infra/redis.py
+++ api/infra/redis.py
@@ -1,5 +1,7 @@
 import os
 import redis
 
+_redis = redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379"))
+
 def get_redis():
-    return redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379"))
+    return _redis
